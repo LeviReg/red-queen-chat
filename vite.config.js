9650e9f58affa4ab7c01092e256766e6afbd4233
Levi Regan <leviarcanine22@gmail.com>
2026-02-26 12:51:52 +0000

    allowed hosts update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  preview: {
+    allowedHosts:[
+      'hp1x8advk09t0f8r8udpwjc7g.js.wpenginepowered.com'
+    ]
+  }
 })
